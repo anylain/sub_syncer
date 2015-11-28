8e1005ebc56c7ccd62be829c1cda19b7a88cd30f
panying <anylain@gmail.com>
2015-11-28 17:40:45 +0800

    优化格式

--- src/sub_syncer.py
+++ src/sub_syncer.py
@@ -1,6 +1,6 @@
 #!/usr/local/bin/python2.7
 # encoding: utf-8
-'''
+"""
 sub_syncer -- shortdesc
 
 sub_syncer is a description
@@ -15,12 +15,11 @@ It defines classes_and_methods
 
 @contact:    user_email
 @deffield    updated: Updated
-'''
+"""
 
 import sys
 import os
 import re
-
 from argparse import ArgumentParser
 from argparse import RawDescriptionHelpFormatter
 
@@ -32,18 +31,23 @@ __updated__ = '2015-01-18'
 TESTRUN = 0
 PROFILE = 0
 
+
 class CLIError(Exception):
-    '''Generic exception to raise and log different fatal errors.'''
+    """Generic exception to raise and log different fatal errors."""
+
     def __init__(self, msg):
         super(CLIError).__init__(type(self))
         self.msg = "E: %s" % msg
+
     def __str__(self):
         return self.msg
+
     def __unicode__(self):
         return self.msg
 
+
 def main(argv=None):  # IGNORE:C0111
-    '''Command line options.'''
+    """Command line options."""
 
     if argv is None:
         argv = sys.argv
@@ -55,7 +59,7 @@ def main(argv=None):  # IGNORE:C0111
     program_build_date = str(__updated__)
     program_version_message = '%%(prog)s %s (%s)' % (program_version, program_build_date)
     program_shortdesc = __import__('__main__').__doc__.split("\n")[1]
-    program_license = '''%s
+    program_license = """%s
 
   Created by user_name on %s.
   Copyright 2015 organization_name. All rights reserved.
@@ -67,48 +71,32 @@ def main(argv=None):  # IGNORE:C0111
   or conditions of any kind, either express or implied.
 
 USAGE
-''' % (program_shortdesc, str(__date__))
+""" % (program_shortdesc, str(__date__))
 
     try:
         # Setup argument parser
         parser = ArgumentParser(description=program_license, formatter_class=RawDescriptionHelpFormatter)
-        parser.add_argument("-r", "--recursive", dest="recurse", action="store_true", help="recurse into subfolders [default: %(default)s]")
-        parser.add_argument("-v", "--verbose", dest="verbose", action="count", help="set verbosity level [default: %(default)s]")
-        parser.add_argument("-i", "--include", dest="include", help="only include paths matching this regex pattern. Note: exclude is given preference over include. [default: %(default)s]", metavar="RE")
-        parser.add_argument("-e", "--exclude", dest="exclude", help="exclude paths matching this regex pattern. [default: %(default)s]", metavar="RE")
         parser.add_argument('-V', '--version', action='version', version=program_version_message)
-        parser.add_argument(dest="path", help="paths to folder(s) with source file(s) [default: %(default)s]", metavar="path")
+        parser.add_argument(dest="path", help="paths to folder(s) with source file(s) [default: %(default)s]",
+                            metavar="path")
 
         # Process arguments
         args = parser.parse_args()
 
         path = args.path
-        verbose = args.verbose
-        recurse = args.recurse
-        inpat = args.include
-        expat = args.exclude
-
-        if verbose > 0:
-            print("Verbose mode on")
-            if recurse:
-                print("Recursive mode on")
-            else:
-                print("Recursive mode off")
-
-        if inpat and expat and inpat == expat:
-            raise CLIError("include and exclude pattern are equal! Nothing will be processed.")
 
         sync_sub(path)
         return 0
     except KeyboardInterrupt:
-        ### handle keyboard interrupt ###
+        # handle keyboard interrupt
         return 0
     except Exception, e:
         indent = len(program_name) * " "
         sys.stderr.write(program_name + ": " + repr(e) + "\n")
         sys.stderr.write(indent + "  for help use --help")
         return 2
-    
+
+
 def sync_sub(path):
     video_info_list = []
     sub_info_list = []
@@ -119,26 +107,29 @@ def sync_sub(path):
         if os.path.isdir(item): continue
         if item.endswith(".mkv"):
             video_info_list.append(init_video_info(item))
-        elif item.endswith(".srt") or item.endswith(".ass") :
+        elif item.endswith(".srt") or item.endswith(".ass"):
             sub_info_list.append(init_sub_info(item))
     for sub_info in sub_info_list:
         match_point = 0;
         hit_video = None
         for video_info in video_info_list:
             new_match_point = check_match_point(sub_info, video_info)
-#             print("[{0}%] {1} >>> {2}".format(new_match_point, sub_info['file_name'], video_info['file_name']))
+            # print("[{0}%] {1} >>> {2}".format(new_match_point, sub_info['file_name'], video_info['file_name']))
             if new_match_point > match_point:
                 hit_video = video_info
                 match_point = new_match_point
             if new_match_point >= 100:
                 break
         if hit_video:
-            print(u"[{0}%] {1} >>> {2}".format(match_point, sub_info['file_name'], hit_video['name'] + "." + sub_info['ext']))
+            print(
+                u"[{0}%] {1} >>> {2}".format(match_point, sub_info['file_name'],
+                                             hit_video['name'] + "." + sub_info['ext']))
             old_name = os.path.join(path, sub_info['file_name'].encode(sys.getfilesystemencoding()))
-            new_name = os.path.join(path, (hit_video['name'] + "." + sub_info['ext']).encode(sys.getfilesystemencoding()))
+            new_name = os.path.join(path,
+                                    (hit_video['name'] + "." + sub_info['ext']).encode(sys.getfilesystemencoding()))
             if old_name != new_name:
                 rename_list.append({'old': old_name, 'new': new_name})
-            
+
     if rename_list:
         choice = raw_input("Rename now ?")
         if choice.lower() in ('y', 'yes'):
@@ -147,7 +138,7 @@ def sync_sub(path):
             print 'All done.'
     else:
         print 'No file need rename.'
-                
+
 
 def init_video_info(file_name):
     m = re.match(r"^(?P<name>.+)\.(?P<ext>[^\.]+)$", file_name)
@@ -159,7 +150,7 @@ def init_video_info(file_name):
             "ext": ext,
             "words": words,
             "file_name": file_name}
-    
+
 
 def init_sub_info(file_name):
     m = re.match(r"^(?P<name>.+)\.(?P<ext>[^\.]+)$", file_name)
@@ -174,11 +165,13 @@ def init_sub_info(file_name):
             "words": words,
             "file_name": file_name}
 
+
 def is_l10n_info(word):
     for key in (u'简', u'繁', u'英', 'chs', 'cht', 'eng'):
         if key in word:
             return True
     return False
+
 
 def check_match_point(sub_info, video_info):
     if sub_info['name'].lower() == video_info['name'].lower():
@@ -186,17 +179,19 @@ def check_match_point(sub_info, video_info):
     word_hit = 0.0
     for word in sub_info['words']:
         if word in video_info['words']:
-            word_hit = word_hit + 1
+            word_hit += 1
     return round((word_hit / len(sub_info['words'])) * 100, 2)
 
 
 if __name__ == "__main__":
     if TESTRUN:
         import doctest
+
         doctest.testmod()
     if PROFILE:
         import cProfile
         import pstats
+
         profile_filename = 'sub_syncer_profile.txt'
         cProfile.run('main()', profile_filename)
         statsfile = open("profile_stats.txt", "wb")
